31dd5659995a1f56a8e7bdedf71079d4c4e46d0f
shita1112 <a1125ts@aiit.ac.jp>
2022-08-12 16:47:29 +0900

    ok

--- frontend/__tests__/index.spec.js
+++
@@ -1,8 +0,0 @@
-import Index from '../pages/index.vue'
-
-describe('pages/index.vue', () => {
-  it('has a default value(empty)', () => {
-    const defaultData = Index.data()
-    expect(defaultData.subTitle).toBe("Zenn is good service!!")
-  })
-})
